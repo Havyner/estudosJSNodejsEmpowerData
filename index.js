aa8cbd396d25ec7b77280859b10c20a2ef327a13
Havyner <havy012_@hotmail.com>
2025-05-25 22:54:48 -0300

    Inacioanlizando estudos

---
+++ index.js
@@ -0,0 +1,24 @@
+/*
+Início
+Faça A valer 5
+Faça B valer 1
+Faça B valer A x B
+A é > do que 1? 
+se sim: Faça A valer A - 1 e volte ao "Faça B valer A x B"
+se não: Imprima o valor de B
+ */
+
+// Comentário de js em documentação
+
+/**
+ * essa função serve para somar
+ * @param {number} num1 primeiro número 
+ * @param {number} num2 segundo número
+ * @returns somatório entre os 2 números
+ */
+
+function soma(num1, num2){
+    return  num1 + num2
+}
+
+soma(5,6)
